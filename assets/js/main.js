f5af551fb7ffc5cc1950d6a7987740eecafd7f83
frgwork <frgwork@ya.ru>
2022-05-27 01:52:33 +0300

    popular swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,17 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
-
+const swiperPopular = new Swiper(".popular__container", {
+   spaceBetween: 32,
+   grabCursor: true,
+   centeredSlide: true,
+   slidesPerView: 'auto',
+   loop: true,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+});
 
 /*=============== VALUE ACCORDION ===============*/
 
